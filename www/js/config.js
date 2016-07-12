d7fe0209ac17430fd0282bebecec5b1f6dea0030
rtfuzz <Fuzz@Afonsos-MacBook-Pro.local>
2016-07-12 12:06:45 -0300

    ap

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={		
+	'ApiUrl': "http://apetitego.com.br/driver/api",
+	'DialogDefaultTitle': "ApetiteGO!",	
+	'PushProjectID': "172997422108",	
+	'APIHasKey': "FuzzSFJDdfsaf3i214utheStarHash"
+};
